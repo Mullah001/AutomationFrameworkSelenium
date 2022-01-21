8de0a0535cdd13d6c7c735d42b63d9fa6365e93c
Hamza Ashfaq <hamza.ashfaq@synavos.com>
2022-01-21 19:58:31 +0500

    Files Sync

--- src/main/pageObjects/LogInPageElements.java
+++
@@ -1,8 +0,0 @@
-package main.pageObjects;
-
-public interface LogInPageElements {
-    String email = "email";
-    String password = "password";
-    String logInButton = "//button[@class='log-submit-btn']";
-    String userLogged = "//span[@id='headerNavUserMenu']//span[@class='username-wrp']";
-}
